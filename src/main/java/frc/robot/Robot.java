2ac234064b533dc641931807307f065bb5dcdcd2
EarthenSkyLaptop <gabegames27@gmail.com>
2019-03-08 17:51:16 -0800

    updated ver and comp changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,10 +8,10 @@ import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 
-import edu.wpi.first.wpilibj.Timer;
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+//import edu.wpi.first.wpilibj.Timer;
 
 public class Robot extends TimedRobot {
     private SpeedControllerGroup left = new SpeedControllerGroup(new Spark(0), new Spark(1));
@@ -20,10 +20,10 @@ public class Robot extends TimedRobot {
 
     private final Joystick m_stick = new Joystick(0);  // Make sure controller is number 0.
 
-    public BuiltInAccelerometer bia = new BuiltInAccelerometer();
+    //public BuiltInAccelerometer bia = new BuiltInAccelerometer();
 
-    public UsbCamera usbCam1;
-    public UsbCamera usbCam2;
+    //public UsbCamera usbCam1;
+    //public UsbCamera usbCam2;
 
     private Spark plugMotor = new Spark(4);
     private Spark clawL = new Spark(5);
@@ -33,28 +33,29 @@ public class Robot extends TimedRobot {
 
     // Component Speeds // 
     
-    public double rampSpeed = 0.25;
-    public double idlePressure = 0.08;
+    public double rampSpeed = 0.4;
 
+    public double idlePressure = 0.24;
     public boolean clawClosed = true;
     public boolean clawButtonToggle = true;
-    public double clawSpeed = 0.5;
+    public double clawSpeed = 0.38;
 
     private boolean speedToggle = false;
-    private double speedMod = 0.6; 
+    private double speedMod = 0.65; 
 
     // Main Functions // 
 
     @Override
     public void robotInit() {
+        CameraServer.getInstance().startAutomaticCapture();
         // Setup the two cameras.
-    	usbCam1 = CameraServer.getInstance().startAutomaticCapture("front-cam", 0);
-        usbCam2 = CameraServer.getInstance().startAutomaticCapture("back-cam", 1);
+    	//usbCam1 = CameraServer.getInstance().startAutomaticCapture("front-cam", 0);
+        //usbCam2 = CameraServer.getInstance().startAutomaticCapture("back-cam", 1);
     }
-    
+
     @Override
     public void autonomousInit() { }
-
+    
     @Override
     public void autonomousPeriodic() {
         teleopPeriodic();  // We are using driver, camera controls for sandstorm.
@@ -65,9 +66,9 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopPeriodic() {
-        m_robotDrive.tankDrive(-m_stick.getY() * speedMod, -m_stick.getRawAxis(5) * speedMod);
+        m_robotDrive.arcadeDrive(-m_stick.getY() * speedMod, m_stick.getX() * speedMod);  // Arcade drive is negitave tank drive.
         
-        // Claw Control --> toggle
+        // Claw Control --> toggle.  Press X for toggle claws.
         if (m_stick.getRawButton(2)==true && clawButtonToggle==true) {  // Case: Switch the motor direction. (in / out)
             clawClosed = !clawClosed;
             clawButtonToggle = false;
@@ -76,46 +77,48 @@ public class Robot extends TimedRobot {
             clawButtonToggle = true;
             if (clawClosed == true) {
                 clawL.setSpeed(-idlePressure);
-                clawR.setSpeed(idlePressure);
+                clawR.setSpeed(idlePressure*0.98);
             } else {
                 clawL.setSpeed(idlePressure);
-                clawR.setSpeed(-idlePressure);
+                clawR.setSpeed(-idlePressure*0.98);
             }
         } 
         
-        // Case: do movement of claw.
+        // Case: do movement of claw.  
         if (m_stick.getRawButton(2)==true && clawButtonToggle==false) { 
             if (clawClosed == true) {
                 clawL.setSpeed(-clawSpeed);
-                clawR.setSpeed(clawSpeed);
+                clawR.setSpeed(clawSpeed*0.98);
             } else {
                 clawL.setSpeed(clawSpeed);
-                clawR.setSpeed(-clawSpeed);
+                clawR.setSpeed(-clawSpeed*0.98);
             }
         }
 
-        // Pull the plug and drop the ramp.
-        if (m_stick.getRawButton(5) && m_stick.getRawButton(6)) {
-            System.out.println("!!DROPPING RAMP!!");
+        // Pull the plug and drop the ramp.  L&R bumpers for dropping ramp.
+        if (m_stick.getRawButton(5)) {
+            //System.out.println("!!DROPPING RAMP!!");
             plugMotor.setSpeed(-rampSpeed);
 
-            m_stick.setRumble(RumbleType.kLeftRumble, 1.0);
-            m_stick.setRumble(RumbleType.kRightRumble, 1.0);
-
+            //m_stick.setRumble(RumbleType.kLeftRumble, 1.0);
+            //m_stick.setRumble(RumbleType.kRightRumble, 1.0);
+        } else if(m_stick.getRawButton(6)) {
+            //System.out.println("!!DROPPING RAMP!!");
+            plugMotor.setSpeed(rampSpeed);
         } else {
             plugMotor.setSpeed(0);
 
-            m_stick.setRumble(RumbleType.kLeftRumble, 0);
-            m_stick.setRumble(RumbleType.kRightRumble, 0);
+            //m_stick.setRumble(RumbleType.kLeftRumble, 0);
+            //m_stick.setRumble(RumbleType.kRightRumble, 0);
         }
         
-        // Toggle for speed.
+        // Toggle for speed.  Triangle toggles speed.
         if (m_stick.getRawButton(4)==true && speedToggle==true) {
             if(speedToggle) {
-                if (speedMod == 0.6) {
+                if (speedMod == 0.65) {
                     speedMod = 0.9;
                 } else {
-                    speedMod = 0.6;
+                    speedMod = 0.65;
                 }
             }
             speedToggle=false;
@@ -127,7 +130,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void testPeriodic() { 
-        g_ticksElapsed++;
-        SmartDashboard.putNumber("Ticks Elapsed--Test", g_ticksElapsed);
+        //g_ticksElapsed++;
+        //SmartDashboard.putNumber("Ticks Elapsed--Test", g_ticksElapsed);
     }
 }
